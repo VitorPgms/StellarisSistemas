fe62700c5ce36d627ce46fb6392261f56d2f771c
VitorPgms <vitor_paulagomes@hotmail.com>
2026-03-31 09:31:50 -0300

    Finalizando tela de login, com autenticação e recuperação de senha.

--- script.js
+++ script.js
@@ -9,19 +9,38 @@ function onChangePassword() {
 }
 
 function login() {
-    firebase.auth().signInWithEmailAndPassword(form.email().value, form.password().value).then(response => {
-        window.location.href = "pages/home/home.html"; /* Navegação entre tela  */
+    showLoading();
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(() => {
+        hideLoading();
+        window.location.href = "pages/home/home.html";
     }).catch(error => {
+        hideLoading();
         alert(getErrorMessage(error));
     });
-    
 }
 
 function getErrorMessage(error) {
     if (error.code === "auth/user-not-found") {
         return "Usuário não encontrado";
     }
+
+    if(error.code === "auth/wrong-password") {
+        return "Senha incorreta";
+    }
     return error.message;
+}
+
+function recoverPassword() {
+    showLoading();
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert("Email de recuperação enviado");
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
 }
 
 function register() {
